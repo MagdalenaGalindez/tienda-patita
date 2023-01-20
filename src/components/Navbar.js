e933d1b2be878b69005ce1f2ae3dac89eac9a7e4
Magalin <galindez.magda@gmail.com>
2023-01-20 13:51:56 -0300

    created navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+const Navbar = () => {
+    return ( <
+        nav >
+        <
+        h1 > Mi App < /h1> <
+        ul >
+        <
+        li > Home < /li> <
+        li > Nosotros < /li> <
+        li > Contacto < /li> <
+        li > Gatos < /li> <
+        li > Veterinaria < /li> </
+        nav >
+    );
+}
+export default Navbar;
