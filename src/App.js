d4e044ab5bd24d0fef3a5397c473f44ece7599c9
Magalin <galindez.magda@gmail.com>
2023-01-24 19:17:08 -0300

    remove React Router

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
-// App.js
 import React from 'react';
 
+import Navbar from './components/Navbar';
+
+
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        Navbar / >
-        <
-        /div>
+    return (
+        <div className="App">
+            <Navbar />
+        </div>
     );
 }
 
-export default App;
-
-// Navbar.js
-
-
-const Navbar = () => {
-    return ( <
-        nav >
-        <
-        h1 > Mi App < /h1> <
-        ul >
-        <
-        li > Home < /li> <
-        li > Nosotros < /li> <
-        li > Contacto < /li> <
-        li > Gatos < /li> <
-        li > Veterinaria < /li> < /
-        ul > <
-        /nav>
-    );
-}
+export default App
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,23 @@
-import { Link } from 'react-router-dom'
+import React from 'react';
+
+
 const Navbar = () => {
     const categories = ['Home', 'Nosotros', 'Contacto', 'Gato', 'Veterinaria'];
 
-    return ( <
-        nav >
-        <
-        div >
-        <
-        h1 > My App < /h1>  <
-        ul > {
-            categories.map((category, index) => ( <
-                li key = { index } >
-                <
-                Link to = { "/${category.toLowerCase()} " } > { category } < /Link>   <
-                /li >
-            ))
-        } <
-        /ul>  < /
-        div > <
-        /nav>
+    return ( 
+        <nav>
+            <div>
+            <h1>My App</h1>
+            <ul>
+                {categories.map((category, index) => (
+                    <li key={index}>
+                        <a href={`/${category.toLowerCase()}`}>{category}</a> 
+                    </li >
+                ))}
+            </ul>
+            </div>
+        </nav>
     );
 }
+
 export default Navbar;
