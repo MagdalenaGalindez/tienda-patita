96e6be6b7689bcb33bceac7894469241d5f7d9e2
Magalin <galindez.magda@gmail.com>
2023-02-05 17:16:02 -0300

    load result with 2 sec delay

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { ItemList } from './ItemList';
 
 
-const ItemListContainer = () => {
-
-  const items = [
+const sampleItems = [
     {
-      'id': 26,
-      'title': 'Plant Hanger For Home',
-      'price': 41,
-      'brand': 'Boho Decor'
+        'id': 26,
+        'title': 'Plant Hanger For Home',
+        'price': 41,
+        'brand': 'Boho Decor'
     },
     {
-      'id': 27,
-      'title': 'Flying Wooden Bird',
-      'price': 51,
-      'brand': 'Flying Wooden'
+        'id': 27,
+        'title': 'Flying Wooden Bird',
+        'price': 51,
+        'brand': 'Flying Wooden'
     }
-  ]
+]
 
-  return (
-    <ItemList items={items} />
-  );
+
+const ItemListContainer = () => {
+
+    const [items, setItems] = useState(null)
+
+    setTimeout(() => {
+        setItems(sampleItems)
+    }, 2000);
+
+
+    if (!items) {
+        return (<strong>Loading...</strong>)
+    }
+
+    return (
+        <ItemList items={items} />
+    );
 }
 
 export default ItemListContainer;
