2444d39889a46a211646e17fa8ccb316729ae810
Magalin <galindez.magda@gmail.com>
2023-02-05 12:30:42 -0300

    add counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ItemListContainer from './components/ItemListContainer'
 import Navbar from './components/Navbar';
-
+import ItemCount from './components/ItemCount';
 
 function App() {
     return (
-        <div className="App">
+        <div>
             <Navbar />
-            <ItemListContainer greeting="holaaa"/>
+            <ItemListContainer greeting="hola Magda"/>
+                <ItemCount /> 
         </div>
     );
 }
