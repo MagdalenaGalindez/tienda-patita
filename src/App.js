a8ae4c1fa1de4b8afec47afcb5a137027ba06c7b
Magalin <galindez.magda@gmail.com>
2023-02-05 21:26:02 -0300

    downgrade React version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ItemListContainer from './components/ItemListContainer'
 import Navbar from './components/Navbar';
-import ItemCount from './components/ItemCount';
 
 
 function App() {
